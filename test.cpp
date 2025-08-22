590775d79b61488b5831a76e7ec10201b44ff7b4
Michael Lee <mikecovlee@163.com>
2025-08-22 17:06:35 +0800

    update test

--- test.cpp
+++ test.cpp
@@ -9,6 +9,9 @@ int main()
 	while (std::getline(std::cin, input)) {
 		gmssl::uint8_array_t input_bytes = gmssl::bytes_encode(input);
 		std::cout << "SM3 Digest: " << gmssl::bytes_decode(gmssl::hex_encode(gmssl::sm3_digest(input_bytes))) << std::endl;
+		gmssl::uint8_array_t ctr_encrypted = gmssl::sm4(gmssl::sm4_mode::ctr_encrypt, key, iv, gmssl::base64_encode(input_bytes));
+		std::cout << "SM4 CTR encrypted: " << gmssl::bytes_decode(gmssl::hex_encode(ctr_encrypted)) << std::endl;
+		std::cout << "SM4 CTR decrypted: " << gmssl::bytes_decode(gmssl::base64_decode(gmssl::sm4(gmssl::sm4_mode::ctr_decrypt, key, iv, ctr_encrypted))) << std::endl;
 		gmssl::uint8_array_t cbc_encrypted = gmssl::sm4(gmssl::sm4_mode::cbc_encrypt, key, iv, gmssl::base64_encode(input_bytes));
 		std::cout << "SM4 CBC encrypted: " << gmssl::bytes_decode(gmssl::hex_encode(cbc_encrypted)) << std::endl;
 		std::cout << "SM4 CBC decrypted: " << gmssl::bytes_decode(gmssl::base64_decode(gmssl::sm4(gmssl::sm4_mode::cbc_decrypt, key, iv, cbc_encrypted))) << std::endl;
